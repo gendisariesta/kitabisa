c385a9e1419bd2b90a773e2c20f1838987eb2040
fikka-aps <fikka.aps21@gmail.com>
2023-04-17 06:42:10 +0700

    commit all

--- kitabisa/views.py
+++ kitabisa/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 from dtks.models import Bansos
 
 def index(request):
-  bansos = Bansos.objects.all()
+  
   context={
     'bansos':bansos
   }
   return render(request, 'landing_page.html', context)
 
 def dashboard(request):
-  return render(request, 'index.html')
+  bansos = Bansos.objects.all()
+  context={
+    'bansos':bansos
+  }
+  return render(request, 'index.html', context)
